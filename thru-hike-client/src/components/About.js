dde819a04784395c196b46235491ba2574e0474a
Nikita Kuckuck <nikitakuckuck@gmail.com>
2022-10-21 11:36:55 -0400

    About page

--- thru-hike-client/src/components/About.js
+++ thru-hike-client/src/components/About.js
@@ -1,7 +1,58 @@
 function About(){
     return(<>
     <h2>About this App</h2>
-    
+    <p>
+        <strong>An app specifically for thru-hikers or section-hikers, to streamline the logistics of town days.</strong>
+    </p>
+    <p>
+        As a long-distance hiker, town days can become the most stressful part of the hike. All you want to do is grab a shower, eat a few thousand calories, and charge your electronics. This app provides a central place for all your town day logistics: reminders, alerts, resupply lists, etc. so you don't have to spend time and energy trying to remember everything you need to do before hitting the trail again.
+    </p>
+    <h3>Future Features:</h3>
+    <p>These features don't exist yet, but they're in the works!</p>
+    <h4>A section summary page, with everything you need for your current town and upcoming section of trail:</h4>
+    <ul>
+        <li>
+            <strong>Area alerts</strong>: links to fire alerts, trail closures, water reports, etc.
+        </li>
+        <li>
+            <strong>Calendar reminders</strong>: customizable alerts to remind you of events that will occur during your next trail section, so you can send out that birthday card before you have to title it as 'belated', or send that important email while you still have cell service. Other features include an alert if you'll hit the next town on a weekend, so you aren't unpleasantly surprised to arrive and find yourself unable to pick up a resupply box from the Post Office, or find last-minute hotels are wildly expensive due to the weekend.
+        </li>
+        <li>
+            <strong>Resupply list</strong>: a place to keep your shopping list, with an option to add reminders for recurring purchases so you don't run out of something important 50 miles from a town.
+        </li>
+        <li>
+            <strong>Town Exit Checklist</strong>: for peace of mind as you hit the trail again.
+        </li>
+        <li>
+            <strong>To-Download</strong>: make sure you don't forget which e-books, apps, playlists, etc you want to download before you leave town.
+        </li>
+        <li>
+            <strong>Cool Town Things</strong>: a spot to dump all your research on trail towns, so when you finally arrive in that town, you won't forget to go check out their world-famous pie, or miss that cool art exhibit.
+        </li>
+        <li>
+            <strong>Cool Trail Things</strong>: a similar format as for town, but for the upcoming trail section. 
+        </li>
+        <li>
+            <strong>Town contacts</strong>: store any PO general delivery addresses, trail angel phone numbers, etc.
+        </li>
+    </ul>
+    <h4>Other General features:</h4>
+    <ul>
+        <li>
+            <strong>Food ideas list</strong>: store all your food ideas here, or add them to the resupply list if you decide to try them out.
+        </li>
+        <li>
+            <strong>Gear list</strong>: a place to keep track of gear you want to try out, or need to get for certain trail sections. You can choose to be alerted during a particular trail section if you need to source this gear for an upcoming section, e.g. for locations that require a bear can, or high elevation trail where you need to pick up extra clothing layers, etc.
+        </li>
+        <li>
+            <strong>General notes</strong>: Create your own categories for notes, with the option to associate notes with a particular trail section so they'll show up on your section summary when you arrive to that section.
+        </li>
+        <li>
+            <strong>Murphy's Day reminders</strong>: basically, an "open in case of emergency" set of notes, in case Murphy's Law shows up in full force. It's a place to keep all the things you want to remind yourself before calling it quits on the whole trail: encouraging quotes, reminders to drink water, etc.
+        </li>
+
+    </ul>
+
     </>)
 }
 export default About;
